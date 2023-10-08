ff76ef710f4ccdb97960b31c38b7cc924ce7c7ae
prathameshpatil5181 <prathameshpatil5181@gmail.com>
2023-10-08 14:47:10 +0530

    changed the mealsSummery

--- src/Components/Meals/MealsSummary.jsx
+++ src/Components/Meals/MealsSummary.jsx
@@ -9,7 +9,7 @@ const MealsSummary = () => {
         and enjoy a delicious lunch or dinner at home.
       </p>
       <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
+        All our meals Delisious and cooked with high-quality ingredients, just-in-time and
         of course by experienced chefs!
       </p>
     </section>
